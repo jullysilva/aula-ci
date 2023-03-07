f25e1efb73048b10933d5fc449f36fbc2a554308
Jully Ketely Alves Da Silva <1181631@sga.pucminas.br>
2023-03-07 19:46:52 -0300

    Adds t e s t to make sure 5 + 5 i s equal to 10

--- main-test.py
+++ main-test.py
@@ -5,5 +5,8 @@ def TestAdd():
         assert Add(2,3) == 5
         print("Add Function works correctly")
 
+        assert Add(5,5) == 10
+        print("Add Function works correctly resulty: 10")
+
 if __name__ == '__main__':
         TestAdd()
