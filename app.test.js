80160369162e83b25cd9eac35698f25f836cd77a
Jully Ketely Alves Da Silva <1181631@sga.pucminas.br>
2023-03-07 20:24:57 -0300

    created projects in javascript

--- app.test.js
+++ app.test.js
@@ -2,7 +2,7 @@
 
 test("TestAdd 5", function(){
     function Add() {
-        equal(Add(2, 3), 5);
+        expect(Add(2, 3)).toBe(5);
     }
 
     Add();
@@ -10,7 +10,7 @@ test("TestAdd 5", function(){
 
 test("TestAdd 10", function(){
     function Add() {
-        equal(Add(5, 5), 10);
+        expect(Add(5, 5)).toBe(10);
     }
 
     Add();
